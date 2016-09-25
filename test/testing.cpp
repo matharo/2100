e7e14d022c464cab00fbf2d19eae0ee90840d228
Lorna Xiao <lornaxiao@hopper.mcs.slu.edu>
2016-09-24 21:42:43 -0500

    git add testing.cpp

--- test/testing.cpp
+++ test/testing.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main(){
 
+int input;
 cout<<"Hello World"<<endl;
+cout<<"whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<<endl;
+cout<<"Enter a number"<<endl;
+cin>>input;
 
+cout<<"The number is " << input<<endl;
 };
