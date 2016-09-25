df6bcd7423c53e9446987dbb7075f0c1b4ebd707
Lorna Xiao <lornaxiao@hopper.mcs.slu.edu>
2016-09-24 21:44:17 -0500

    testing.cpp

--- test/testing.cpp
+++ test/testing.cpp
@@ -5,8 +5,5 @@ int main(){
 int input;
 cout<<"Hello World"<<endl;
 cout<<"whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"<<endl;
-cout<<"Enter a number"<<endl;
-cin>>input;
 
-cout<<"The number is " << input<<endl;
 };
